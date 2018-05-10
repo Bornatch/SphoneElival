2ec959c7783ae7e927630800081cd2512e5e46fa
Bornatch <valentin.bornatici@gmail.com>
2018-05-10 12:18:51 +0200

    Insertion scrollPane de gallerie

--- SphoneEliVal/src/gui/FrameGallerie.java
+++ SphoneEliVal/src/gui/FrameGallerie.java
@@ -2,7 +2,10 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.Scrollbar;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -13,6 +16,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 public class FrameGallerie extends FramePrincipale {
 
@@ -22,7 +26,7 @@ public class FrameGallerie extends FramePrincipale {
 	// objet dossier
 	private File dossier = new File("./Gallerie/");
 	File[] fichier = dossier.listFiles();
-	int i ;
+	String index;
 
 	// liste de boutons adaptable selon nombre de photos
 	List<JButton> miniatures = new ArrayList<JButton>();
@@ -31,19 +35,26 @@ public class FrameGallerie extends FramePrincipale {
 
 		// ajout de panel de présentation des images
 
-		this.add(panelCenter, BorderLayout.CENTER);
+		// this.add(panelCenter, BorderLayout.CENTER);
 		panelCenter.setBackground(Color.DARK_GRAY);
-		panelCenter.setLayout(new GridLayout(fichier.length / 3, 3, 10, 10));
+		panelCenter.setLayout(new GridLayout(0, 3, 10, 10));
+		// panelCenter.setLayout(new FlowLayout());
 		// panelCenter.setLayout(new GridBagLayout());
-		panelCenter.setBounds(20, 40, 440, 700);
+		panelCenter.setBounds(20, 40, 440, fichier.length / 3 * 100);
 
 		// Créer une liste des Imagesicon, le -2 retire le fichier thumbs
-		for ( i = 0; i < fichier.length -2; i++) {
+		for (int i = 0; i < fichier.length - 1; i++) {
 			miniatures.add(new JButton(new ImageIcon(fichier[i].getPath())));
 			panelCenter.add(miniatures.get(i));
+			miniatures.get(i).setName("" + i);
+			miniatures.get(i).setPreferredSize(new Dimension(100, 100));
 			miniatures.get(i).addActionListener(new TraitementImage());
 			// System.out.println(liste[i].getPath());
 		}
+
+		JScrollPane scrollPane = new JScrollPane(panelCenter);
+		this.add(scrollPane, BorderLayout.CENTER);
+		scrollPane.setBounds(20, 40, 440, 700);
 
 	}
 
@@ -52,11 +63,11 @@ public class FrameGallerie extends FramePrincipale {
 		 * comportement du bouton d'images,
 		 */
 		public void actionPerformed(ActionEvent e) {
-			
-			JFrame image = new FrameImage(((JButton)e.getSource()).getIcon());
-//			JFrame image = new FrameImage(fichier, i);
-//			System.out.println(i);
-//			image.setVisible(true);
+
+			// JFrame image = new FrameImage(((JButton)e.getSource()).getIcon());
+			JFrame image = new FrameImage(fichier, Integer.parseInt(((JButton) e.getSource()).getName()));
+
+			image.setVisible(true);
 
 			dispose();
 		}
--- SphoneEliVal/src/gui/FrameImage.java
+++ SphoneEliVal/src/gui/FrameImage.java
@@ -2,11 +2,14 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -19,16 +22,22 @@ public class FrameImage extends FramePrincipale {
 	private JPanel navigation = new JPanel();
 
 	Icon image;
-	
-	int i;
+	File[] fichier;
+
+	int index;
 
 	private JButton next = new JButton(new ImageIcon("./icon/next.png"));
 	private JButton cancel = new JButton(new ImageIcon("./icon/cancel.png"));
 	private JButton back = new JButton(new ImageIcon("./icon/back.png"));
 
-	public FrameImage(Icon icon) {
+	public FrameImage(File[] fichier, int index) {
+		this.fichier = fichier;
+		this.index = index;
+		this.image = new ImageIcon(fichier[index].getPath());
+		
+		//test
+		System.out.println(fichier[index].getPath());
 
-		this.image = icon;
 		imageSelect = new JLabel(image);
 
 		// panel d'affichage des images sur partie haute
@@ -42,62 +51,106 @@ public class FrameImage extends FramePrincipale {
 		navigation.setBackground(Color.DARK_GRAY);
 
 		// ajout du boutons de navigation
-		navigation.add(back);
-		back.setBounds(0, 0, 143, 100);
-		back.setOpaque(true);
-		back.setContentAreaFilled(false);
-		back.setBorderPainted(false);
+		if (index < fichier.length - 2) { // suppression de Thumbs
+			navigation.add(next);
+			next.setBounds(285, 0, 143, 100);
+			next.setOpaque(true);
+			next.setContentAreaFilled(false);
+			next.setBorderPainted(false);
+			next.addActionListener(new TraitementNext());
+		}
+		if (index != 0) {
+			navigation.add(back);
+			back.setBounds(0, 0, 143, 100);
+			back.setOpaque(true);
+			back.setContentAreaFilled(false);
+			back.setBorderPainted(false);
+			back.addActionListener(new TraitementBack());
+		}
 
 		navigation.add(cancel);
 		cancel.setBounds(143, 0, 142, 100);
 		cancel.setOpaque(true);
 		cancel.setContentAreaFilled(false);
 		cancel.setBorderPainted(false);
-
-		navigation.add(next);
-		next.setBounds(285, 0, 143, 100);
-		next.setOpaque(true);
-		next.setContentAreaFilled(false);
-		next.setBorderPainted(false);
+		cancel.addActionListener(new TraitementCancel());
 
 	}
 
-	public FrameImage(File[] fichier, int i) {
-		File[] listeNom =fichier;
-		this.i=i;
-		this.image = new ImageIcon(listeNom[i].getPath());
-		System.out.println(listeNom[i].getPath());
-		
-		imageSelect = new JLabel(image);
+	public class TraitementNext implements ActionListener {
+		/**
+		 * comportement du bouton suivant (next),
+		 */
+		public void actionPerformed(ActionEvent e) {
 
-		// panel d'affichage des images sur partie haute
-		this.add(imageSelect, BorderLayout.CENTER);
-		imageSelect.setBounds(20, 40, 440, 600);
+			JFrame image = new FrameImage(fichier, index + 1);
+			image.setVisible(true);
 
-		// panel d'affichage des boutons de navigation
-		this.add(navigation, BorderLayout.CENTER);
-		navigation.setLayout(null);
-		navigation.setBounds(20, 640, 440, 100);
-		navigation.setBackground(Color.DARK_GRAY);
+			dispose();
+		}
+	}
 
-		// ajout du boutons de navigation
-		navigation.add(back);
-		back.setBounds(0, 0, 143, 100);
-		back.setOpaque(true);
-		back.setContentAreaFilled(false);
-		back.setBorderPainted(false);
+	public class TraitementBack implements ActionListener {
+		/**
+		 * comportement du bouton précédent (back),
+		 */
+		public void actionPerformed(ActionEvent e) {
 
-		navigation.add(cancel);
-		cancel.setBounds(143, 0, 142, 100);
-		cancel.setOpaque(true);
-		cancel.setContentAreaFilled(false);
-		cancel.setBorderPainted(false);
+			JFrame image = new FrameImage(fichier, index - 1);
+			image.setVisible(true);
 
-		navigation.add(next);
-		next.setBounds(285, 0, 143, 100);
-		next.setOpaque(true);
-		next.setContentAreaFilled(false);
-		next.setBorderPainted(false);
+			dispose();
+		}
+	}
+
+	public class TraitementCancel implements ActionListener {
+		/**
+		 * comportement du bouton retour (cancel),
+		 */
+		public void actionPerformed(ActionEvent e) {
+
+			JFrame gallerie = new FrameGallerie();
+			gallerie.setVisible(true);
+
+			dispose();
+
+		}
 	}
 
 }
+
+// public FrameImage(Icon icon) {
+//
+// this.image = icon;
+// imageSelect = new JLabel(image);
+//
+// // panel d'affichage des images sur partie haute
+// this.add(imageSelect, BorderLayout.CENTER);
+// imageSelect.setBounds(20, 40, 440, 600);
+//
+// // panel d'affichage des boutons de navigation
+// this.add(navigation, BorderLayout.CENTER);
+// navigation.setLayout(null);
+// navigation.setBounds(20, 640, 440, 100);
+// navigation.setBackground(Color.DARK_GRAY);
+//
+// // ajout du boutons de navigation
+// navigation.add(back);
+// back.setBounds(0, 0, 143, 100);
+// back.setOpaque(true);
+// back.setContentAreaFilled(false);
+// back.setBorderPainted(false);
+//
+// navigation.add(cancel);
+// cancel.setBounds(143, 0, 142, 100);
+// cancel.setOpaque(true);
+// cancel.setContentAreaFilled(false);
+// cancel.setBorderPainted(false);
+//
+// navigation.add(next);
+// next.setBounds(285, 0, 143, 100);
+// next.setOpaque(true);
+// next.setContentAreaFilled(false);
+// next.setBorderPainted(false);
+//
+// }
