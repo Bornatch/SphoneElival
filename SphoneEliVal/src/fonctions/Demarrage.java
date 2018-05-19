8047b2b31321474916b3f5b95466e101ccb1b867
Bornatch <valentin.bornatici@gmail.com>
2018-05-19 13:54:35 +0200

    test contact

--- SphoneEliVal/src/fonctions/Demarrage.java
+++ SphoneEliVal/src/fonctions/Demarrage.java
@@ -2,6 +2,7 @@ package fonctions;
 
 import javax.swing.JFrame;
 
+import BDContacts.Contacts;
 import gui.FrameAccueil;
 
 public class Demarrage {
@@ -10,8 +11,10 @@ public class Demarrage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		JFrame test = new FrameAccueil() ;
+		//JFrame test = new FrameAccueil() ;
 		//System.out.println("test git");
+		
+		Contacts vb = new Contacts("Val", "Bonratch", "2017854", "2541", "2551");
 
 	}
 
