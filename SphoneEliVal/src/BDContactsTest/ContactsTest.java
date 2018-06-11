4c062edb239e482f98951f3e388f86ded20ddc0c
elizabeth <elizabeth@elizabeth-HP>
2018-06-12 01:36:42 +0200

    test

--- SphoneEliVal/src/BDContactsTest/ContactsTest.java
+++ SphoneEliVal/src/BDContactsTest/ContactsTest.java
@@ -26,7 +26,7 @@ class ContactsTest {
 	@Test
 	void testafficherList() {
 		
-		Contacts c =new Contacts ("Eli","Bitschnau","0781254565","0782540210","test@hevs.ch", null);
+		Contacts c =new Contacts ("Eli","Bitschnau","0781254565","0782540210","test@hevs.ch",null);
 		ArrayList<Contacts> contactsList = new ArrayList<Contacts>();
 		
 		for (int i = 0; i < 5; i++) { //Remplir la list
