ee162d63abbdb9685352fcef1a4aa2561e6873e6
elizabeth <elizabeth@elizabeth-HP>
2018-06-12 01:33:43 +0200

    Correction et test

--- SphoneEliVal/src/BDContactsTest/ContactsTest.java
+++ SphoneEliVal/src/BDContactsTest/ContactsTest.java
@@ -1,29 +1,16 @@
 package BDContactsTest;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.IOException;
-import java.nio.file.DirectoryNotEmptyException;
-import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Path;
-import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.List;
-
-import javax.swing.JFormattedTextField;
-import javax.swing.text.MaskFormatter;
 
 import org.junit.jupiter.api.Test;
 
 import BDContacts.Contacts;
 
-@SuppressWarnings("unused")
 class ContactsTest {
 
-	
 	@Test
 	void testContacts() { //Methode ok
 		 Contacts c=new Contacts("Eli","Bitschnau","0781254565","0782540210","test@hevs.ch", null);
@@ -34,13 +21,13 @@ class ContactsTest {
 		 assertNotNull("L'objet n'existe pas",c);//si l'objet n'existe pas 
 		 
 	}
-
-		
+	
+	
 	@Test
 	void testafficherList() {
 		
 		Contacts c =new Contacts ("Eli","Bitschnau","0781254565","0782540210","test@hevs.ch", null);
-		List<Contacts> contactsList = new ArrayList<Contacts>();
+		ArrayList<Contacts> contactsList = new ArrayList<Contacts>();
 		
 		for (int i = 0; i < 5; i++) { //Remplir la list
 			contactsList.add(c);
@@ -48,6 +35,5 @@ class ContactsTest {
 		assertNotNull("La liste est vide",c);
 		//fail("Not yet implemented");
 	}
-
 	
 }
